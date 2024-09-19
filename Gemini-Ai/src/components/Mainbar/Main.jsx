0782323a7d19ddf536bbd2d004a3a98a9e9b9ad1
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-19 12:57:22 +0530

    sidebar completed

--- Gemini-Ai/src/components/Mainbar/Main.jsx
+++ Gemini-Ai/src/components/Mainbar/Main.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import {Context} from '../../context/Context'
+
 const Main = () => {
-  return (
+
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+    return (
     <div className="main">
       <div className="nav">
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
-      <div className="main-contanier">
-        <div className="greet">
+            <div className="main-contanier">
+                {!showResult ? <>
+                  <div className="greet">
           <p>
             <span>Hello,Friend</span>
           </p>
@@ -33,13 +47,26 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+                </>
+                    : <div className='result'>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{ recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            
+                        </div>
+                </div>    
+                
+            }
+      
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter a prompt here" />
+            <input type="text" onChange={(e)=>setInput(e.target.value)} value={input} placeholder="Enter a prompt here" />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
             </div>
           </div>
           <p className="bottom-info">
